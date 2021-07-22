522a79a66b69b89f66c8b9f55a3a6f84a79456b7
Meaghan <meaghan.m.doherty@uconn.edu>
2021-07-22 19:08:08 -0400

    Fully implemented the menu screens
    Fuck yeah!!

---
+++ Assets/Devin/Scripts/OpenMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenuButton : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.gameObject.SetActive(!pauseMenu.gameObject.activeSelf);
+        }
+    }
+}
